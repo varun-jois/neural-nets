NeuralNet.py - Fixed the softmax function implementation. nn_script.py - Added a multiclass problem.

NeuralNet.py
@@ -93,7 +93,8 @@ class NeuralNet(object):
         :param array: A numpy array.
         :return: A numpy array.
         """
-        return np.exp(array)/np.sum(np.exp(array))
+        array_adj = array - np.max(array)
+        return np.exp(array_adj)/np.sum(np.exp(array_adj), axis=0, keepdims=True)
 
     def _function(self, activation, prime=False):
         functions_dict = {
@@ -194,26 +195,28 @@ class NeuralNet(object):
         else:
             parameters['A' + str(layers)] = f('softmax')(parameters['Z' + str(layers)])
 
-    def _compute_cost(self, y, lambd=0, e=1e-10):
-        m = y.shape[1]
+    def _compute_cost(self, y, lambd=0):
+        r, m = y.shape
         layers = self.details['layers']
         parameters = self.parameters
 
         yhat = parameters['A' + str(layers)]
-        cost = -np.sum(y * np.log(yhat + e) + (1 - y) * np.log(1 - yhat + e)) / m + \
-                lambd * sum(np.sum(parameters['W' + str(l + 1)] ** 2) for l in range(layers)) / (2 * m)
+        if r == 1:
+            cost = -np.sum(y * np.log(yhat) + (1 - y) * np.log(1 - yhat)) / m + \
+                    lambd * sum(np.sum(parameters['W' + str(l + 1)] ** 2) for l in range(layers)) / (2 * m)
+        else:
+            cost = -np.sum(y * np.log(yhat)) / m + \
+                   lambd * sum(np.sum(parameters['W' + str(l + 1)] ** 2) for l in range(layers)) / (2 * m)
         return cost
 
     def _back_prop(self, x, y, activation):
         parameters = self.parameters
         f = self._function
         layers = self.details['layers']
-        layer_sizes = self.details['layer_sizes']
         lambd = self.hyper_parameters['lambd']
 
         m = y.shape[1]
-        if layer_sizes[-1] == 1:
-            parameters['dZ' + str(layers)] = parameters['A' + str(layers)] - y
+        parameters['dZ' + str(layers)] = parameters['A' + str(layers)] - y
         for l in reversed(range(layers - 1)):
             parameters['dW' + str(l + 2)] = \
                 parameters['dZ' + str(l + 2)].dot(parameters['A' + str(l + 1)].T) / m + \

nn_script.py
@@ -1,41 +1,3 @@
-from neural_net import NeuralNet
-#from NeuralNet import NeuralNet
-from helper_functions import *
-import sklearn
-from sklearn import datasets
-import matplotlib.pyplot as plt
-import numpy as np
-import copy
-import _pickle
-
-np.random.seed(0)
-X, y = sklearn.datasets.make_moons(1000, noise=0.20)
-#plt.scatter(X[:,0], X[:,1], s=10, c=y, cmap=plt.cm.Spectral)
-
-X = X.T
-Y = y.reshape(1, 1000)
-mu, var = get_normalisation_constants(X)
-X = (X - mu) / var
-# X = X[:, :2]
-# Y = Y[:, :2]
-
-np.random.seed(0)
-nn = NeuralNet()
-nn.initialize_weights(2, [3,3,3,3,3,3,1], initializer='xavier')
-nn.train_batch(X, Y, epochs=1000, alpha=1, activation='tanh', lambd=1, grad_check=True, dropout_keep_prob=1)
-# nn.train_mini_batch(X, Y, epochs=2000, alpha=.001, activation='tanh', lambd=0, grad_check=True, mini_batch_size=16,
-#                     optimizer='adam')
-p1 = nn.parameters
-with open('p1.p', 'wb') as f:
-    _pickle.dump(p1, f)
-
-p, err = nn.predict(X, Y)
-print(err)
-np.sum(p==Y)
-nn.epochs
-
-###############################################
-
 from NeuralNet import NeuralNet, get_normalisation_constants
 import sklearn
 from sklearn import datasets
@@ -44,17 +6,21 @@ import numpy as np
 import _pickle
 
 np.random.seed(0)
-X, y = sklearn.datasets.make_moons(1000, noise=0.20)
-#plt.scatter(X[:,0], X[:,1], s=10, c=y, cmap=plt.cm.Spectral)
+X, y = sklearn.datasets.make_moons(1000, noise=0.2)
+plt.scatter(X[:,0], X[:,1], s=10, c=y)
+# X, y = sklearn.datasets.make_blobs(1000, n_features=2, centers=3, random_state=2)
+# plt.scatter(X[:,0], X[:,1], s=10, c=y)
+lb = sklearn.preprocessing.LabelBinarizer()
+
 X = X.T
-Y = y.reshape(1, 1000)
+Y = lb.fit_transform(y).T
 mu, var = get_normalisation_constants(X)
 X = (X - mu) / var
 
 np.random.seed(0)
 nn = NeuralNet()
-nn.initializer_xavier(2, [3,3,3,3,3,3,1])
-nn.gd_batch(X, Y, epochs=1000, alpha=1, grad_check=True, lambd=1)
+nn.initializer_xavier(2, [5, 5, 1])
+nn.gd_batch(X, Y, epochs=3000, alpha=1, grad_check=True, lambd=0, activation='tanh')
 p2 = nn.parameters
 
 p, err = nn.predict(X, Y)