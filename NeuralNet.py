Fixed the issue with gradient checking.

NeuralNet.py
@@ -13,6 +13,7 @@ def get_normalisation_constants(array):
     variance = np.sum(array ** 2) / m
     return mean, variance
 
+
 # The NeuralNet Class
 class NeuralNet(object):
     def __init__(self):
@@ -280,8 +281,8 @@ class NeuralNet(object):
 
             # Gradient checking
             if grad_check is True and dropout_keep_prob == 1 and i < 10:
-                grad_arrays = [parameters['dW' + str(i + 1)] for i in range(layers)]
-                grad_arrays.extend([parameters['dB' + str(i + 1)] for i in range(layers)])
+                grad_arrays = [self.parameters['dW' + str(i + 1)] for i in range(layers)]
+                grad_arrays.extend([self.parameters['dB' + str(i + 1)] for i in range(layers)])
                 grads = np.array([]).reshape(1, -1)
                 for a in grad_arrays:
                     grads = np.concatenate((grads, a.reshape(1, -1)), axis=1)

nn_script.py
@@ -6,23 +6,28 @@ from sklearn import datasets
 import matplotlib.pyplot as plt
 import numpy as np
 import copy
+import _pickle
 
 np.random.seed(0)
-X, y = sklearn.datasets.make_moons(200, noise=0.20)
+X, y = sklearn.datasets.make_moons(1000, noise=0.20)
 #plt.scatter(X[:,0], X[:,1], s=10, c=y, cmap=plt.cm.Spectral)
 
 X = X.T
-Y = y.reshape(1, 200)
+Y = y.reshape(1, 1000)
 mu, var = get_normalisation_constants(X)
 X = (X - mu) / var
 # X = X[:, :2]
 # Y = Y[:, :2]
 
+np.random.seed(0)
 nn = NeuralNet()
-nn.initialize_weights(2, [7, 2, 1], initializer='xavier')
-nn.train_batch(X, Y, epochs=2000, alpha=1, activation='tanh', lambd=0, grad_check=True)
+nn.initialize_weights(2, [3,3,3,3,3,3,1], initializer='xavier')
+nn.train_batch(X, Y, epochs=1000, alpha=1, activation='tanh', lambd=1, grad_check=True, dropout_keep_prob=1)
 # nn.train_mini_batch(X, Y, epochs=2000, alpha=.001, activation='tanh', lambd=0, grad_check=True, mini_batch_size=16,
 #                     optimizer='adam')
+p1 = nn.parameters
+with open('p1.p', 'wb') as f:
+    _pickle.dump(p1, f)
 
 p, err = nn.predict(X, Y)
 print(err)
@@ -36,22 +41,29 @@ import sklearn
 from sklearn import datasets
 import matplotlib.pyplot as plt
 import numpy as np
+import _pickle
 
 np.random.seed(0)
-X, y = sklearn.datasets.make_moons(200, noise=0.20)
+X, y = sklearn.datasets.make_moons(1000, noise=0.20)
 #plt.scatter(X[:,0], X[:,1], s=10, c=y, cmap=plt.cm.Spectral)
 X = X.T
-Y = y.reshape(1, 200)
+Y = y.reshape(1, 1000)
 mu, var = get_normalisation_constants(X)
 X = (X - mu) / var
 
-np.random.seed(3)
+np.random.seed(0)
 nn = NeuralNet()
-nn.initializer_xavier(2, [3,3,1])
-nn.gd_batch(X, Y, epochs=2000, alpha=1, grad_check=True)
+nn.initializer_xavier(2, [3,3,3,3,3,3,1])
+nn.gd_batch(X, Y, epochs=1000, alpha=1, grad_check=True, lambd=1)
+p2 = nn.parameters
+
 p, err = nn.predict(X, Y)
 print(err)
 
+with open('p1.p', 'rb') as f:
+    p1 = _pickle.load(f)
+
+all([np.array_equal(p1[a], p2[b]) for a, b in zip(sorted(list(p1.keys())), sorted(list(p2.keys())))])
 
 
 